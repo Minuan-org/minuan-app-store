cd13915aeb5cf3d3f7d0971718a608d13508b1d8
IamBobo <abdulsamad.arilewola@gmail.com>
2026-05-17 12:04:10 +0100

    Change BrowserRouter basename to '/minuan-app-store'

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export default function App() {
   return (
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/minuan-app-store">
       <Routes>
       </Routes>
     </BrowserRouter>
